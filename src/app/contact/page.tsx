590d046695cd67b7553ae4f1aa66a50a9fc1fa4f
Jay.develops <jaymations1234@gmail.com>
2024-01-04 17:39:05 -0800

    put metadata on the bottom of the contact file

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,13 +4,8 @@ import ContactHeader from "@/components/Contact/ContactHeader/ContactHeader"
 import {Metadata} from "next";
 import Container from "@/components/Container";
 
-export const metadata: Metadata = {
-    title: 'Contact',
-    description: 'Experienced Full-Stack Software Engineer based in Los Angeles, California, offering expertise in Next.js, ReactJS, PHP, and WordPress. Over three years of hands-on experience developing robust and scalable solutions for diverse projects. Contact Jesus Perez for your software development needs.'
-}
 
 export default function Contact() {
-
     return (
         <div className="my-12">
             <Container>
@@ -19,4 +14,8 @@ export default function Contact() {
             </Container>
         </div>
     )
+}
+export const metadata: Metadata = {
+    title: 'Contact',
+    description: 'Experienced Full-Stack Software Engineer based in Los Angeles, California, offering expertise in Next.js, ReactJS, PHP, and WordPress. Over three years of hands-on experience developing robust and scalable solutions for diverse projects. Contact Jesus Perez for your software development needs.'
 }
