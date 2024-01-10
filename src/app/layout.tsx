52ebb4d686d22ff3ea303c0553a68f46292fdc6a
Jay.develops <jaymations1234@gmail.com>
2024-01-09 20:39:41 -0800

    added more metadata to root layout.tsx page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,19 +7,53 @@ import dynamic from "next/dynamic"
 
 const inter = Inter({ subsets: ['latin'] })
 const DynamicNavigation: React.ComponentType<{}> = dynamic(() => import("@/components/Navigation/Navigation"))
+const descriptionText: string =
+        'Unlock the potential of your business needs with Jesus Perez Arias, ' +
+        'a seasoned professional software engineer with extensive expertise in crafting exceptional FullStack and Mobile Applications. ' +
+        'Jesus is open for hire in full-time or freelance opportunities, with his ' +
+        'rich background spanning three years, Jesus specializes in full-stack technologies, including React, React Native, Next.js, TypeScript, MongoDB, and Node.js. ' +
+        'Known for strong communication skills in technology topics and educator, Jesus is the ideal person for bringing your visionary projects to life.'
 
 export const metadata: Metadata = {
   title: {
     default: 'Jesus Perez Developer Portfolio',
     template: `%s | Jesus Perez Developer Portfolio`
   },
-  description:
-      'Unlock the potential of your business needs with Jesus Perez Arias, ' +
-      'a seasoned professional software engineer with extensive expertise in crafting exceptional FullStack and Mobile Applications. ' +
-      'Jesus is open for hire in full-time or freelance opportunities, with his ' +
-      'rich background spanning three years, Jesus specializes in full-stack technologies, including React, React Native, Next.js, TypeScript, MongoDB, and Node.js. ' +
-      'Known for strong communication skills in technology topics and educator, Jesus is the ideal person for bringing your visionary projects to life.',
+  description: descriptionText,
+  category: 'technology',
+  keywords: [
+      'Next.js',
+    'React',
+    'JavaScript',
+      'Developer',
+      'Portfolio',
+      'MongoDB',
+      'SQL',
+      'Fullstack',
+      'Web Development',
+      'Mobile Development',
+  ],
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+    },
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Jesus Perez Developer Portfolio',
+    description: descriptionText,
+    creator: '@jay_develops_',
+    images: ['https://jesusperez.dev/og.png'], // Must be an absolute URL
+  },
 }
+
 
 export default function RootLayout({
   children,
