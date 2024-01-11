371c41e86d7c0fcac70fa67d587e1ab6699aa645
Jay.develops <jaymations1234@gmail.com>
2024-01-10 16:36:22 -0800

    Removed static delay from testing UX in /page.tsx component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Hero from "@/components/Home/Hero/Hero"
-import delay from "delay"
 export default async function Home() {
-    await delay(3000)
     return (
         <section className="mt-28 md:mt-8 px-24 md:px-8">
             <Hero />
